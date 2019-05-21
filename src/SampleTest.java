2702bb34f527b1066c53101f175486d3a2fb1519
mkrishna0690 <mkrishna0690@gmail.com>
2019-05-21 07:06:53 -0400

    Sample changes

---
+++ src/SampleTest.java
@@ -0,0 +1,14 @@
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+
+		String hungry = "Yes";
+		if(hungry=="Yes") {
+			System.out.println("Some thing to eat");
+		}else {
+			System.out.println("Not");
+		}
+	}
+
+}
